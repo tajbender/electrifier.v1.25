23c8d0042e6a815fe714b824bf009d0b580d22ca
Thorsten Jung <tajbender@users.noreply.github.com>
2024-11-11 10:22:44 +0100

    Add `Shell32ItemArray` Add `ReadOnlyShell32ItemArray` (subclass of `Vanara.Windows.Shell.ShellItemArray`)

--- src/electrifier/Controls/Vanara/ExplorerBrowserItem.cs
+++ src/electrifier/Controls/Vanara/ExplorerBrowserItem.cs
@@ -1,4 +1,5 @@
-﻿using electrifier.Controls.Vanara.Services;
+﻿using System.Collections;
+using electrifier.Controls.Vanara.Services;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -121,9 +122,84 @@ public static class ExplorerBrowserItemExtensions
         if (itm.Children.Count > 0)
         {
             itm.Children.Clear();
-
         }
 
         return itm;
     }
+}
+
+/// <summary>
+/// Wrapper for 
+///     Vanara.Windows.Shell.ShellItemArray
+/// Implementing
+///     System.Collections.IList
+/// </summary>
+public class ReadOnlyShell32ItemArray : ShellItemArray, IList
+{
+    object? IList.this[int index]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+    public bool IsFixedSize => true;
+    public bool IsReadOnly => true;
+    public bool IsSynchronized => throw new NotImplementedException();
+    public object SyncRoot => throw new NotImplementedException();
+    public int Add(object? value) => throw new NotImplementedException();
+    public void Clear() => throw new NotImplementedException();
+    public bool Contains(object? value)
+    {
+        return false;
+    }
+    public void CopyTo(Array array, int index)
+    {
+    }
+    public int IndexOf(object? value)
+    {
+        return -1;
+    }
+    public void Insert(int index, object? value) => throw new NotImplementedException();
+    public void Remove(object? value) => throw new NotImplementedException();
+    public void RemoveAt(int index) => throw new NotImplementedException();
+
+    // TODO event CollectionChanged, add Creator/Sender (TreeViewItem, ListViewItem) - attribute
+    // TODO event iconchanged
+    // todo from(epBrowserItem), from(ReadableShellItemArray), for(explorerbrowser) etc..
+}
+
+
+/// <summary>
+/// Wrapper for 
+///     Vanara.Windows.Shell.ShellItemArray
+/// Implementing
+///     System.Collections.IList
+///
+/// TODO: Add version index
+/// </summary>
+public class Shell32ItemArray : IList
+{
+    public Shell32ItemArray() { }
+
+    object? IList.this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    bool IList.IsFixedSize => false;
+
+    bool IList.IsReadOnly => false;
+
+    int ICollection.Count => throw new NotImplementedException();
+
+    bool ICollection.IsSynchronized => throw new NotImplementedException();
+
+    object ICollection.SyncRoot => throw new NotImplementedException();
+
+    int IList.Add(object? value) => throw new NotImplementedException();
+    void IList.Clear() => throw new NotImplementedException();
+    bool IList.Contains(object? value) => throw new NotImplementedException();
+    void ICollection.CopyTo(Array array, int index) => throw new NotImplementedException();
+    IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    int IList.IndexOf(object? value) => throw new NotImplementedException();
+    void IList.Insert(int index, object? value) => throw new NotImplementedException();
+    void IList.Remove(object? value) => throw new NotImplementedException();
+    void IList.RemoveAt(int index) => throw new NotImplementedException();
 }
