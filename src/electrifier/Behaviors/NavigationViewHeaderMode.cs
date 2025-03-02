ab0dd8df416f56b7cb1f09ff1bc5279f73afbd41
Thorsten Jung <tajbender@users.noreply.github.com>
2025-03-02 19:31:04 +0100

    Cleanup `Reconfigure` Branch
    Cleanup for new `Reconfigure` Branch

--- src/electrifier/Behaviors/NavigationViewHeaderMode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace electrifier.Behaviors;
-
-public enum NavigationViewHeaderMode
-{
-    Always,
-    Never,
-    Minimal
-}
--- src/electrifier/Controls/Vanara/ShellContextMenu.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace electrifier.Controls.Vanara;
-
-public class ShellContextMenu;
