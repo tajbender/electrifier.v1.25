706e4c11f93221c3ac93cea66d1d37d171367fbe
Thorsten Jung <tajbender@users.noreply.github.com>
2025-03-07 11:14:26 +0100

    Refactor ShellListView and ShellNamespaceTreeControl
    Updated ShellListView.xaml to clarify ScrollView usage and added a comment for future DataTable integration.
    In ShellNamespaceTreeControl.xaml.cs, added a using directive, simplified the constructor, and improved the SelectionChanged property to handle item selection events correctly.

--- src/electrifier/Controls/ShellNamespaceTreeControl.xaml.cs
+++ src/electrifier/Controls/ShellNamespaceTreeControl.xaml.cs
@@ -12,23 +12,37 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using electrifier.Controls.Helpers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace electrifier.Controls
-{
-    public sealed partial class ShellNamespaceTreeControl : UserControl
-    {
-        public ShellNamespaceTreeControl()
-        {
-            this.InitializeComponent();
-        }
+namespace electrifier.Controls;
 
-        public Action<object, SelectionChangedEventArgs> SelectionChanged
+public sealed partial class ShellNamespaceTreeControl : UserControl
+{
+    public ShellNamespaceTreeControl()
+    {
+        this.InitializeComponent();
+
+        this.SelectionChanged = (sender, e) =>
         {
-            get;
-            internal set;
-        }
+            if (e.AddedItems.Count > 0)
+            {
+                var item = e.AddedItems[0] as BrowserItem;
+
+                if (item != null)
+                {
+                    var args = new SelectionChangedEventArgs(Array.Empty<object>(), Array.Empty<object>());
+                    SelectionChanged(this, args);
+                }
+            }
+        };
+    }
+
+    public Action<object, SelectionChangedEventArgs> SelectionChanged
+    {
+        get;
+        internal set;
     }
 }
