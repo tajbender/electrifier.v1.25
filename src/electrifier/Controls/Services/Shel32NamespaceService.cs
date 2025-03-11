89240d44fb95d8dcb5b4da25b81ffc31b767e0d3
Thorsten Jung <tajbender@users.noreply.github.com>
2025-03-11 09:42:21 +0100

    Add TODO for Shell Log-Writer and improve GetDebuggerDisplay
    - Added a comment for a Shell Log-Writer implementation.
    - Updated `GetDebuggerDisplay` to return an empty string if `ToString()` is null, improving robustness.

--- src/electrifier/Controls/Services/Shel32NamespaceService.cs
+++ src/electrifier/Controls/Services/Shel32NamespaceService.cs
@@ -7,11 +7,23 @@ using System.Threading.Tasks;
 
 namespace electrifier.Controls.Services;
 
+// TODO:
+// WARN: Add Shell Log-Writer class, for logging Shell32 operations
+
+/// <summary>
+/// A service for interacting with the Shell32 namespace.
+/// </summary>
 [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 internal class Shel32NamespaceService
 {
+
+
+    /// <summary>
+    /// Gets the debugger display.
+    /// </summary>
+    /// <returns>A <see cref="string" represnteation of this object for debugging purposes./></returns>
     private string GetDebuggerDisplay()
     {
-        return ToString();
+        return ToString() ?? string.Empty;
     }
 }
