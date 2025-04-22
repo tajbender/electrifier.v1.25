f7ae56b817b78a42d32a6365e8c8ff36b4da3fd7
Thorsten Jung <tajbender@users.noreply.github.com>
2025-04-22 14:07:44 +0200

    Add ExplorerBrowserPaneDataSource record
    Introduce a new internal record `ExplorerBrowserPaneDataSource` in the `electrifier.Controls.Models` namespace. This record implements `INotifyPropertyChanged` and encapsulates data for a shell item, including properties for `DisplayName`, `ExplicitDisplayName`, and `Items`. A constructor is provided for easy instantiation using a `ShellItem`. Necessary using directives are also added.

---
+++ src/electrifier/Controls/Models/ExplorerBrowserPaneDataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanara.Windows.Shell;
+
+namespace electrifier.Controls.Models;
+
+internal record ExplorerBrowserPaneDataSource(ShellItem ShellItem
+, string? ExplicitDisplayName
+, IEnumerable<ShellItem>? Items
+) : INotifyPropertyChanged
+{
+    public string DisplayName
+    {
+        get => ExplicitDisplayName ?? ShellItem.Name;
+    }
+    public string? ExplicitDisplayName
+    {
+        get; set;
+    } = ExplicitDisplayName;
+    public IEnumerable<ShellItem>? Items
+    {
+        get; set;
+    } = Items;
+
+    public ExplorerBrowserPaneDataSource(ShellItem shellItem) : this(shellItem, shellItem.Name, []) { }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+}
