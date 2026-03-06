eca9c88076c456c832e43b8ad63996751b651ea4
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-06 03:16:15 +0100

    Remove Controls\Models\ from project; add Spy Mode button
    Removed all Controls\Models\ files from build and packaging in electrifier.csproj. Added a disabled "Spy Mode - Chiffre filnames" button to ExplorerBrowser.xaml as a placeholder. Cleaned up unused usings in BrowserItemFactory.cs.

--- src/electrifier/Controls/Helpers/BrowserItemFactory.cs
+++ src/electrifier/Controls/Helpers/BrowserItemFactory.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using electrifier.Controls.Contracts;
 using electrifier.Controls.Services;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Vanara.PInvoke;
 using Vanara.Windows.Shell;
-using Windows.Graphics.Imaging;
-using static Vanara.PInvoke.Shell32;
 
 namespace electrifier.Controls.Helpers;
 
